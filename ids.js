f6bac27347853e64927f4a522687590159effbe2
Ana Borges <paulasimiqueli@live.com>
2021-05-06 11:38:37 -0300

    Add files via upload

---
+++ ids.js
@@ -0,0 +1,22 @@
+(function(){
+    var ids= {
+        "store_id": "store",
+        "product_id": "product", 
+        "cart_id": "cart",
+        "order_id": "order",
+        "customer_id": "customer"
+    } 
+    
+    var str = "Alguns IDs importantes: \n"
+    for (const key in ids) {
+        if (ids.hasOwnProperty(key)) {
+            if(LS[ids[key]]){
+                str  = str + key + ": " + LS[ids[key]].id + "\n"
+            
+            }
+            
+        }
+    }
+    alert(str)
+    
+})();
