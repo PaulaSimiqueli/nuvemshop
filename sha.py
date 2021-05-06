d3724e65006357baf470c29de8a982a80d354d13
Ana Borges <paulasimiqueli@live.com>
2021-05-06 13:35:58 -0300

    Add files via upload

---
+++ sha.py
@@ -0,0 +1,24 @@
+#python3
+
+import hmac
+import hashlib
+import json
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+secret = "UD8HDcHU7aoZAkDg43XhriHLHYUje8V92W3EmOuz7sC4xTx2"
+
+
+@app.route('/webhook', methods=['POST'])
+def respond():
+    print(request.data)
+    byte_key = bytes(secret, 'UTF-8')  
+    message = request.data
+    h = hmac.new(byte_key, message, hashlib.sha256).hexdigest()
+
+    header = request.headers.get('x-linkedstore-hmac-sha256')
+    if header == h:
+        return Response(status=200)
+    else:
+        return Response(status=401)
