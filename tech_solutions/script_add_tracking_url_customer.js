1653c9518fba4bdcf991ff08ec3a92612856d047
Ana Paula Simiqueli Borges <ana.simiqueli@MacBook-Pro-de-Ana.local>
2021-06-07 13:53:41 -0300

    change var to let

--- tech_solutions/script_add_tracking_url_customer.js
+++ tech_solutions/script_add_tracking_url_customer.js
@@ -5,8 +5,8 @@
  */
     if (window.location.pathname.includes('/account/orders')) {
 
-        var account = document.getElementsByClassName("account-page");
-        var section = account.item(0)
+        let account = document.getElementsByClassName("account-page");
+        let section = account.item(0)
         if (section)
             if (section.innerText.indexOf("Frete: Enviado") !== -1) {
                 orderDetail = section.children.item(0)
@@ -16,13 +16,13 @@
                 //Searching for the order id.
                 data_store = orderDetail.getAttribute("data-store")
                 ids = data_store.split("-")
-                var url = "http://example.com"
+                let url = "http://example.com"
                 if (ids.length == 4) {
                     url = "http://example.com?tracking-id=" + ids[3]
                 }
                 //Creating the tag to add the link.
-                var link = document.createElement('a');
-                var linkText = document.createTextNode("RASTREAR PEDIDO");
+                let link = document.createElement('a');
+                let linkText = document.createTextNode("RASTREAR PEDIDO");
                 link.appendChild(linkText);
                 link.title = "RASTREAR";
                 link.href = url;
